d7be83bf361507b1face438435f484dde52fe1ec
matteofab10 <matteofabio10@gmail.com>
2022-01-21 17:02:46 +0100

    footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,66 @@
 <footer>
-  {{-- footer --}}
+  <div class="container footer-top">
+    <div class="list">
+      <div class="column-list">
+        <h3>DC COMICS</h3>
+        <ul class="footer-list">
+          <li><a href="">Characters</a></li>
+          <li><a href="">Games</a></li>
+          <li><a href="">Comics</a></li>
+          <li><a href="">Movies</a></li>
+          <li><a href="">Films</a></li>
+          <li><a href="">News</a></li>
+          <li><a href="">TV</a></li>
+        </ul>
+
+        <h3>SHOP</h3>
+        <ul class="footer-list">
+          <li><a href="">Shop DC</a></li>
+          <li><a href="">Shop DC Collectibles</a></li>
+        </ul>
+      </div>
+      <div class="column-list">
+        <h3>DC</h3>
+        <ul class="footer-list">
+          <li><a href="">Terms Of Use</a></li>
+          <li><a href="">Privacy policy(New)</a></li>
+          <li><a href="">Ad Choices</a></li>
+          <li><a href="">Advertising</a></li>
+          <li><a href="">Jobs</a></li>
+          <li><a href="">Subscriptions</a></li>
+          <li><a href="">Talent Workshops</a></li>
+          <li><a href="">CPSC Certificates</a></li>
+          <li><a href="">Ratings</a></li>
+          <li><a href="">Shop Help</a></li>
+          <li><a href="">Contact Us</a></li>
+        </ul>
+      </div>
+      <div class="column-list">
+        <h3>SITES</h3>
+        <ul class="footer-list">
+          <li><a href="">DC</a></li>
+          <li><a href="">MAD Magazine</a></li>
+          <li><a href="">DC Kids</a></li>
+          <li><a href="">DC Universe</a></li>
+          <li><a href="">DC Power Visa</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="footer-img">
+      <img src="/img/dc-logo-bg.png" alt="dc-logo">
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="container">
+      <button>SIGN-UP NOW!</button>
+      <div class="footer-social">
+        <h3>FOLLOW US</h3>
+        <img src="/img/footer-facebook.png" alt="facebook">
+        <img src="/img/footer-twitter.png" alt="twitter">
+        <img src="/img/footer-youtube.png" alt="youtube">
+        <img src="/img/footer-pinterest.png" alt="pinterest">
+        <img src="/img/footer-periscope.png" alt="periscope">
+      </div>
+    </div>
+  </div>
 </footer>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,6 +1,6 @@
 <header>
   <div class="container">
-    <img src="{{asset('img/dc-logo.png')}}" alt="dc-logo">
+    <img src="{{asset('/img/dc-logo.png')}}" alt="dc-logo">
     <ul>
       @foreach ($nav as $link)
       <li><a> {{ $link['text'] }} </a></li> 
